2568648441afe33c4f49d6faec993f8dfdc242e6
zkj <438101845@qq.com>
2020-02-08 14:25:36 +0800

    [zkj]-course3-String.format

--- 005_extends/src/main/java/Application.java
+++ 005_extends/src/main/java/Application.java
@@ -12,7 +12,8 @@ public class Application {
         Eagles eagles = new Eagles(4, 5, new String[]{"兔子"});
         System.out.printf(String.format("一只%d岁的老鹰，体重%dKg，这只老鹰生活在%s窝所以它只捕食%s。",
                 eagles.getAge(), eagles.getWeight(), eagles.getPrey()[0], eagles.getPrey()[0] ));
-        System.out.printf("某天它先%s，在饥肠辘辘之际%s了一只%s，然后%s一顿，", eagles.fly(), eagles.hunt(), eagles.getPrey()[0], eagles.eat());
-        System.out.printf("心满意足之后就%s了。", eagles.sleep());
+        System.out.printf(String.format("某天它先%s，在饥肠辘辘之际%s了一只%s，然后%s一顿，",
+                eagles.fly(), eagles.hunt(), eagles.getPrey()[0], eagles.eat()));
+        System.out.printf(String.format("心满意足之后就%s了。", eagles.sleep()));
     }
 }
